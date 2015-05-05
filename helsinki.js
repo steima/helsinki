2ab14b587407ea589fe32dc903576f74f6a56843
steima <matthias@matthias.mbp.local>
2015-05-05 23:04:46 +0200

    First prototype

--- helsinki.js
+++ helsinki.js
@@ -8,6 +8,7 @@ var HELSINKI = HELSINKI || {};
  * This is called by the framework after widget was successfully loaded
  */ 
 function helsinki_startup() {
+	HELSINKI.registerDeviceMotion();
 	console.log('Helsinki started');
 };
 
@@ -19,25 +20,69 @@ function helsinki_teardown() {
 };
 
 /**
+ * Class that handles deviceorientation events
+ */
+var OrientationHandler = function() {
+	this.tilt = -1;
+	this.orientation = 0;
+
+	/** update the data and fire events if necessary */
+	this.update = function(tilt, orientation) {
+		tilt = HELSINKI.round(tilt);
+		this.orientation = 360 - HELSINKI.round(orientation);
+		var tBool = tilt >= 45;
+		if(this.tilt != tBool) {
+			this.tilt = tBool;
+			this.updateUi();
+			console.log('change view');
+		}
+		if(this.tilt > 0) {
+			var scrollContentWidth = $('#scrollContent').width();
+			var factor = scrollContentWidth / 360;
+			var pos = Math.round(this.orientation * factor);
+			console.log('pos ' + pos);
+			$('#scrollPane').scrollLeft(pos);
+		}
+	};
+
+	/** switch the UI according to current tilt */
+	this.updateUi = function() {
+		if(this.tilt > 0) {
+			$('#treeSixtyDegree').show();
+			$('#mapView').hide();
+		}else{
+			$('#treeSixtyDegree').hide();
+			$('#mapView').show();
+		}
+	};
+};
+
+/**
  * Register device motion events for this widget
  */
 HELSINKI.registerDeviceMotion = function() {
 	if(window.DeviceMotionEvent) {
-  		$.on('devicemotion', function(me) {
-			var accel = me.acceleration;
-			var accelGrav = me.accelerationIncludingGravity;
-			var rotation = me.rotationRate;
+		HELSINKI.oHandler = new OrientationHandler();
+		console.log('device supports device motion event');
+		$('#alpha').html('test');
+		window.addEventListener('deviceorientation', function(oe) {
+			$('#alpha').html('a ' + HELSINKI.round(oe.alpha));
+			$('#gamma').html('c ' + HELSINKI.round(oe.gamma));
+			HELSINKI.oHandler.update(oe.gamma, oe.alpha);
+		}, false);
 
-			console.log('x accel: ' + accel.x);
-			console.log('a rotation: ' + rotation.alpha);
-		});
 	}else{
 		alert('Sorry your device does not support motion events');
 	}
 };
 
-
-
+/**
+ * Helper for rounding numbers
+ */
+HELSINKI.round = function (val) {
+	var amt = 10;
+	return Math.round(val * amt) /  amt;
+}
 
 /**
  * The following will run the startup code in cases where we are not 
